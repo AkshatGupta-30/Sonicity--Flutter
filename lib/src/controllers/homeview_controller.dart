2a2d249296531ff7135de4acc46eb789e291cbb6
Akshat Gupta <akshatgupta0030@gmail.com>
2024-02-25 18:02:58 +0530

    Trending section tab bar in home controller

--- lib/src/controllers/homeview_controller.dart
+++ lib/src/controllers/homeview_controller.dart
@@ -1,18 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sonicity/src/models/home.dart';
 import 'package:sonicity/src/services/home_view_api.dart';
 
-class HomeViewController extends GetxController {
+class HomeViewController extends GetxController with GetSingleTickerProviderStateMixin {
   final home = Home.empty().obs;
+  late TabController tabController;
+  final selectedTab = 0.obs;
 
   @override
-  void onReady() {
-    getHome();
-    super.onReady();
+  void onInit() {
+    super.onInit();
+    tabController = TabController(vsync: this, length: 2);
+    tabController.addListener(() {
+      selectedTab.value = tabController.index;
+    });
+    getHomeData();
   }
 
-  void getHome() async {
+  void getHomeData() async {
     Home data = await HomeViewApi.get();
     home.value = data;
+  }
+
+  @override
+  void onClose() {
+    tabController.dispose();
+    super.onClose();
   }
 }
--- lib/utils/sections/trending_now_section.dart
+++ lib/utils/sections/trending_now_section.dart
@@ -3,6 +3,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:iconify_flutter_plus/iconify_flutter_plus.dart';
+import 'package:iconify_flutter_plus/icons/ic.dart';
+import 'package:iconify_flutter_plus/icons/ph.dart';
 import 'package:sonicity/src/controllers/homeview_controller.dart';
 import 'package:sonicity/src/models/album.dart';
 import 'package:sonicity/src/models/song.dart';
@@ -12,24 +15,11 @@ import 'package:sonicity/utils/widgets/shimmer_widget.dart';
 import 'package:sonicity/utils/widgets/song_widget.dart';
 import 'package:sonicity/utils/sections/title_section.dart';
 
-class TrendingNowSection extends StatefulWidget {
+class TrendingNowSection extends StatelessWidget {
   final Size media;
   final HomeViewController homeController;
 
   const TrendingNowSection({super.key, required this.media, required this.homeController});
-
-  @override
-  State<TrendingNowSection> createState() => _TrendingNowSectionState();
-}
-
-class _TrendingNowSectionState extends State<TrendingNowSection> with SingleTickerProviderStateMixin {
-  late TabController tabController;
-
-  @override
-  void initState() {
-    tabController = TabController(length: 2, vsync: this);
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +29,11 @@ class _TrendingNowSectionState extends State<TrendingNowSection> with SingleTick
           children: [
             TitleSection(title: "Trending Now", center: true),
             TabBar(
-              controller: tabController,
+              controller: homeController.tabController,
               indicatorColor: Colors.red,
               dividerColor: Colors.red.withOpacity(0.5),
               overlayColor: MaterialStatePropertyAll(Colors.transparent),
               splashFactory: NoSplash.splashFactory,
-              labelColor: accentColor.withOpacity(0.75), unselectedLabelColor: accentColorDark,
               isScrollable: false,
               physics: NeverScrollableScrollPhysics(),
               tabs: [
@@ -52,68 +41,89 @@ class _TrendingNowSectionState extends State<TrendingNowSection> with SingleTick
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(Icons.music_note, size: 25),
+                      Iconify(
+                        Ph.music_note_duotone, size: 25,
+                        color: (homeController.selectedTab.value == 0) ? accentColor : accentColorDark,
+                      ),
                       SizedBox(width: 8),
-                      Text("Music", style: TextStyle(fontSize: 21))
+                      Text(
+                        "Music",
+                        style: TextStyle(
+                          fontSize: 21,
+                          color: (homeController.selectedTab.value == 0) ? accentColor : accentColorDark
+                        )
+                      )
                     ],
                   ),
                 ),
                 Tab(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.album, size: 25),
-                      SizedBox(width: 8),
-                      Text("Album", style: TextStyle(fontSize: 21))
-                    ],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Iconify(
+                          Ic.twotone_album, size: 25,
+                          color: (homeController.selectedTab.value == 1) ? accentColor : accentColorDark,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                            "Album",
+                            style: TextStyle(
+                              fontSize: 21,
+                              color: (homeController.selectedTab.value == 1) ? accentColor : accentColorDark
+                          )
+                        ),
+                      ],
+                    ),
                   ),
-                )
               ]
             ),
             SizedBox(height: 20),
             SizedBox(
-              height: widget.media.width/1.25,
+              height: media.width/1.25,
               child: TabBarView(
-                controller: tabController,
+                controller: homeController.tabController,
                 physics: NeverScrollableScrollPhysics(),
                 children: [
                   CarouselSlider.builder(
-                    itemCount: (widget.homeController.home.value.trendingNow.songs.isEmpty)
+                    itemCount: (homeController.home.value.trendingNow.songs.isEmpty)
                       ? 1
-                      : widget.homeController.home.value.trendingNow.songs.length,
+                      : homeController.home.value.trendingNow.songs.length,
                     itemBuilder: (context, index, realIndex) {
-                      if(widget.homeController.home.value.trendingNow.songs.isEmpty) {
+                      if(homeController.home.value.trendingNow.songs.isEmpty) {
                         return ShimmerCard();
-                      } else if(widget.homeController.home.value.trendingNow.songs.length == 1) {
-                        return SongCard(song: widget.homeController.home.value.trendingNow.songs.first);
+                      } else if(homeController.home.value.trendingNow.songs.length == 1) {
+                        return SongCard(song: homeController.home.value.trendingNow.songs.first);
                       }
-                      Song song = widget.homeController.home.value.trendingNow.songs[index];
+                      Song song = homeController.home.value.trendingNow.songs[index];
                       return SongCard(song: song);
                     },
                     options: CarouselOptions(
-                      height: widget.media.width/1.25,
+                      height: media.width/1.25,
                       autoPlay: true, autoPlayAnimationDuration: Duration(milliseconds: 700), autoPlayInterval: Duration(seconds: 8),
                       initialPage: 0, enlargeCenterPage: true,
-                      enableInfiniteScroll: (widget.homeController.home.value.trendingNow.songs.length != 1)
+                      enableInfiniteScroll: (homeController.home.value.trendingNow.songs.length != 1)
                     ),
                   ),
                   CarouselSlider.builder(
-                    itemCount: (widget.homeController.home.value.trendingNow.albums.isEmpty)
-                      ? 1
-                      : widget.homeController.home.value.trendingNow.albums.length,
-                    itemBuilder: (context, index, realIndex) {
-                      if(widget.homeController.home.value.trendingNow.albums.isEmpty) {
-                        return ShimmerCard();
-                      }
-                      Album album = widget.homeController.home.value.trendingNow.albums[index];
-                      return AlbumCard(album: album);
-                    },
-                    options: CarouselOptions(
-                      height: widget.media.width/1.25,
-                      autoPlay: true, autoPlayAnimationDuration: Duration(milliseconds: 700), autoPlayInterval: Duration(seconds: 8),
-                      initialPage: 0, enlargeCenterPage: true,
+                      itemCount: (homeController.home.value.trendingNow.albums.isEmpty)
+                        ? 1
+                        : homeController.home.value.trendingNow.albums.length,
+                      itemBuilder: (context, index, realIndex) {
+                        if(homeController.home.value.trendingNow.albums.isEmpty) {
+                          return ShimmerCard();
+                        } else if(homeController.home.value.trendingNow.albums.length == 1) {
+                          return AlbumCard(album: homeController.home.value.trendingNow.albums.first);
+                        }
+                        Album album = homeController.home.value.trendingNow.albums[index];
+                        return AlbumCard(album: album);
+                      },
+                      options: CarouselOptions(
+                        height: media.width/1.25,
+                        autoPlay: true, autoPlayAnimationDuration: Duration(milliseconds: 700), autoPlayInterval: Duration(seconds: 8),
+                        initialPage: 0, enlargeCenterPage: true,
+                        enableInfiniteScroll: (homeController.home.value.trendingNow.albums.length != 1)
+                      ),
                     ),
-                  ),
                 ],
               ),
             ),
