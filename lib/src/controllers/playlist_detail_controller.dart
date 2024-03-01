73537f1381302f6d97451528d57307a6f4c87fa5
Akshat Gupta <akshatgupta0030@gmail.com>
2024-03-01 19:10:21 +0530

    Added sorting of songs in playlist

--- lib/src/controllers/playlist_detail_controller.dart
+++ lib/src/controllers/playlist_detail_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:sonicity/src/models/playlist.dart';
 import 'package:sonicity/src/services/playlist_details_api.dart';
+import 'package:sonicity/utils/contants/enums.dart';
 
 class PlaylistDetailController extends GetxController {
   final String playlistId;
@@ -17,5 +18,22 @@ class PlaylistDetailController extends GetxController {
   void getDetails() async {
     Playlist music = await PlaylistDetailsApi.get(playlistId);
     playlist.value = music;
+  }
+
+  void sort(SortType sortType, Sort sortBy) {
+    if(sortType == SortType.name) {
+      (sortBy == Sort.asc)
+        ? playlist.value.songs!.sort((a, b) => a.name.compareTo(b.name))
+        : playlist.value.songs!.sort((a, b) => b.name.compareTo(a.name));
+    } else if(sortType == SortType.duration) {
+      (sortBy == Sort.asc)
+        ? playlist.value.songs!.sort((a, b) => int.parse(a.duration!).compareTo(int.parse(b.duration!)))
+        : playlist.value.songs!.sort((a, b) => int.parse(b.duration!).compareTo(int.parse(a.duration!)));
+    } else {
+      (sortBy == Sort.asc)
+        ? playlist.value.songs!.sort((a, b) => a.year!.compareTo(b.year!))
+        : playlist.value.songs!.sort((a, b) => b.year!.compareTo(a.year!));
+    }
+    update();
   }
 }
--- lib/src/views/details/playlist_details_view.dart
+++ lib/src/views/details/playlist_details_view.dart
@@ -17,6 +17,7 @@ import 'package:sonicity/src/models/playlist.dart';
 import 'package:sonicity/src/models/song.dart';
 import 'package:sonicity/src/views/todo/todo_view.dart';
 import 'package:sonicity/utils/contants/colors.dart';
+import 'package:sonicity/utils/contants/enums.dart';
 import 'package:sonicity/utils/widgets/report_widget.dart';
 import 'package:sonicity/utils/widgets/pop_up_buttons.dart';
 import 'package:sonicity/utils/widgets/song_widget.dart';
@@ -52,140 +53,8 @@ class PlaylistDetailsView extends StatelessWidget {
                 }
                 return CustomScrollView(
                   slivers: [
-                    SliverAppBar(
-                      pinned: true, floating: false, snap: false,
-                      toolbarHeight: kBottomNavigationBarHeight,
-                      shadowColor: Colors.black87, surfaceTintColor: Colors.black87, backgroundColor: Colors.grey.shade900,
-                      leading: BackButton(color: Colors.white),
-                      expandedHeight: 400,
-                      flexibleSpace: FlexibleSpaceBar(
-                        centerTitle: true, expandedTitleScale: 1.5,
-                        stretchModes: [StretchMode.blurBackground],
-                        titlePadding: EdgeInsets.only(left: 20, right: 20, bottom: 75),
-                        title: SizedBox(
-                          width: media.width/1.4,
-                          child: Text(
-                            playlist.name, maxLines: 1, overflow: TextOverflow.ellipsis, textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        background: Stack(
-                          children: [
-                            CachedNetworkImage(
-                              imageUrl: playlist.image.highQuality, fit: BoxFit.fill,
-                              width: double.maxFinite, height: 380,
-                              placeholder: (context, url) {
-                                return Image.asset("assets/images/appLogo150x150.png", fit: BoxFit.fill);
-                              },
-                              errorWidget: (context, url, error) {
-                                return Image.asset("assets/images/appLogo150x150.png", fit: BoxFit.fill);
-                              },
-                            ),
-                            Container(
-                              width: media.width, height: 420,
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  colors: [Colors.black.withOpacity(0), Colors.black.withOpacity(0.75)],
-                                  begin: Alignment.center, end: Alignment.bottomCenter,
-                                )
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      actions: [
-                        SpiderReport(color: Colors.redAccent),
-                        Gap(10),
-                        PopupMenuButton(
-                          itemBuilder: (context) {
-                            return [
-                              PopupMenuItem(
-                                onTap: () {
-                                  Get.to(() => ToDoView(text: "Sorting of songs"));
-                                },
-                                child: PopUpButtonRow(icon: Mdi.sort_alphabetical_ascending, label: "Name Asc")
-                              ),
-                              PopupMenuItem(
-                                child: PopUpButtonRow(icon: Mdi.sort_alphabetical_descending, label: "Name Desc")
-                              ),
-                              PopupMenuItem(
-                                child: PopUpButtonRow(icon: Mdi.sort_numeric_ascending, label: "Duration Asc")
-                              ),
-                              PopupMenuItem(
-                                child: PopUpButtonRow(icon: Mdi.sort_numeric_descending, label: "Duration Desc")
-                              ),
-                              PopupMenuItem(
-                                child: PopUpButtonRow(icon: Mdi.sort_calendar_ascending, label: "Year Asc")
-                              ),
-                              PopupMenuItem(
-                                child: PopUpButtonRow(icon: Mdi.sort_calendar_descending, label: "Year Desc")
-                              ),
-                            ];
-                          },
-                          icon: Iconify(MaterialSymbols.sort_rounded, color: Colors.white),
-                          position: PopupMenuPosition.under, color: Colors.grey.shade900,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                        ),
-                        Gap(10)
-                      ],
-                      bottom: PreferredSize(
-                        preferredSize: Size(double.maxFinite, 60),
-                        child: Container(
-                          color: Colors.grey.shade900,
-                          child: Row( 
-                            children: [
-                              Gap(20),
-                                Text(
-                                "${playlist.songs!.length} Songs",
-                                style: TextStyle(color: Colors.white, fontSize: 21),
-                              ),
-                              Spacer(),
-                              Container(
-                                height: kBottomNavigationBarHeight, alignment: Alignment.center,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "Shuffle",
-                                          style: TextStyle(color: Colors.grey.shade300, fontSize: 21),
-                                        ),
-                                        Iconify(Ic.twotone_shuffle, color: Colors.grey.shade300, size: 25),
-                                      ],
-                                    ),
-                                    Gap(5),
-                                    Container(height: 30, width: 1, color: Colors.white38),
-                                    Gap(5),
-                                    Container(
-                                      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                                      decoration: BoxDecoration(
-                                        border: Border.all(color: Colors.grey.shade300),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      child: Iconify(Ic.twotone_play_arrow, color: Colors.grey.shade300, size: 27),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Gap(10)
-                            ],
-                          ),
-                        )
-                      ),
-                    ),
-                    SliverPadding(
-                      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-                      sliver: SliverList.builder(
-                        itemCount: playlist.songs!.length,
-                        itemBuilder: (context, index) {
-                          Song song = playlist.songs![index];
-                          return SongsRow(song: song);
-                        },
-                      ),
-                    ),
+                    _appBar(media, playlist, controller),
+                    _playlistSongs(playlist),
                   ],
                 );
               }
@@ -194,50 +63,196 @@ class PlaylistDetailsView extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: ExpandableFab.location,
-      floatingActionButton: ExpandableFab(
-        duration: Duration(milliseconds: 250),
-        distance: 100.0,
-        type: ExpandableFabType.fan,
-        pos: ExpandableFabPos.right,
-        childrenOffset: Offset(0,0),
-        fanAngle: 60,
-        openButtonBuilder: RotateFloatingActionButtonBuilder(
-          child: Iconify(IconParkTwotone.more_four, color: accentColor),
-          fabSize: ExpandableFabSize.regular,
-          foregroundColor: accentColor,
-          backgroundColor: accentColorDark,
-          shape: CircleBorder(),
-          angle: 3.14 * 2,
-        ),
-        closeButtonBuilder: FloatingActionButtonBuilder(
-          size: 56,
-          builder: (BuildContext context, void Function()? onPressed, Animation<double> progress) {
-            return IconButton(
-              onPressed: onPressed,
-              icon: Iconify(AntDesign.close_circle_twotone, size: 40, color: accentColor),
-            );
-          },
-        ),
-        overlayStyle: ExpandableFabOverlayStyle(blur: 5),
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              Get.to(() => ToDoView(text: "Add this playlist to starred"));
-            },
-            tooltip: "Add to Playlist",
-            backgroundColor: accentColorDark, shape: CircleBorder(),
-            child: Iconify(MaterialSymbols.star_outline_rounded, color: accentColor, size: 40)
+      floatingActionButton: _floatingActionButton(),
+    );
+  }
+
+  SliverAppBar _appBar(Size media, Playlist playlist, PlaylistDetailController controller) {
+    return SliverAppBar(
+      pinned: true, floating: false, snap: false,
+      toolbarHeight: kBottomNavigationBarHeight,
+      shadowColor: Colors.black87, surfaceTintColor: Colors.black87, backgroundColor: Colors.grey.shade900,
+      leading: BackButton(color: Colors.white),
+      expandedHeight: 400,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true, expandedTitleScale: 1.5,
+        stretchModes: [StretchMode.blurBackground],
+        titlePadding: EdgeInsets.only(left: 20, right: 20, bottom: 75),
+        title: SizedBox(
+          width: media.width/1.4,
+          child: Text(
+            playlist.name, maxLines: 1, overflow: TextOverflow.ellipsis, textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
           ),
-          FloatingActionButton(
-            onPressed: () {
-              Get.to(() => ToDoView(text: "Add this playlist to library"));
-            },
-            tooltip: "Add to Queue",
-            backgroundColor: accentColorDark, shape: CircleBorder(),
-            child: Iconify(MaterialSymbols.add, color: accentColor, size: 40)
-          ),
-        ],
+        ),
+        background: Stack(
+          children: [
+            CachedNetworkImage(
+              imageUrl: playlist.image.highQuality, fit: BoxFit.fill,
+              width: double.maxFinite, height: 380,
+              placeholder: (context, url) {
+                return Image.asset("assets/images/appLogo150x150.png", fit: BoxFit.fill);
+              },
+              errorWidget: (context, url, error) {
+                return Image.asset("assets/images/appLogo150x150.png", fit: BoxFit.fill);
+              },
+            ),
+            Container(
+              width: media.width, height: 420,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.black.withOpacity(0), Colors.black.withOpacity(0.75)],
+                  begin: Alignment.center, end: Alignment.bottomCenter,
+                )
+              ),
+            )
+          ],
+        ),
       ),
+      actions: [
+        SpiderReport(color: Colors.redAccent),
+        Gap(10),
+      ],
+      bottom: PreferredSize(
+        preferredSize: Size(double.maxFinite, 60),
+        child: Container(
+          color: Colors.grey.shade900,
+          child: Row( 
+            children: [
+              Gap(20),
+                Text(
+                "${playlist.songs!.length} Songs",
+                style: TextStyle(color: Colors.white, fontSize: 21),
+              ),
+              Spacer(),
+              Container(
+                height: kBottomNavigationBarHeight, alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Shuffle",
+                          style: TextStyle(color: Colors.grey.shade300, fontSize: 21),
+                        ),
+                        Iconify(Ic.twotone_shuffle, color: Colors.grey.shade300, size: 25),
+                      ],
+                    ),
+                    Gap(5),
+                    Container(height: 30, width: 1, color: Colors.white38),
+                    Gap(5),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Iconify(Ic.twotone_play_arrow, color: Colors.grey.shade300, size: 27),
+                    ),
+                    PopupMenuButton(
+                      itemBuilder: (context) {
+                        return [
+                          PopupMenuItem(
+                            onTap: () => controller.sort(SortType.name, Sort.asc),
+                            child: PopUpButtonRow(icon: Mdi.sort_alphabetical_ascending, label: "Name Asc")
+                          ),
+                          PopupMenuItem(
+                            onTap: () => controller.sort(SortType.name, Sort.dsc),
+                            child: PopUpButtonRow(icon: Mdi.sort_alphabetical_descending, label: "Name Desc")
+                          ),
+                          PopupMenuItem(
+                            onTap: () => controller.sort(SortType.duration, Sort.asc),
+                            child: PopUpButtonRow(icon: Mdi.sort_numeric_ascending, label: "Duration Asc")
+                          ),
+                          PopupMenuItem(
+                            onTap: () => controller.sort(SortType.duration, Sort.dsc),
+                            child: PopUpButtonRow(icon: Mdi.sort_numeric_descending, label: "Duration Desc")
+                          ),
+                          PopupMenuItem(
+                            onTap: () => controller.sort(SortType.year, Sort.asc),
+                            child: PopUpButtonRow(icon: Mdi.sort_calendar_ascending, label: "Year Asc")
+                          ),
+                          PopupMenuItem(
+                            onTap: () => controller.sort(SortType.year, Sort.dsc),
+                            child: PopUpButtonRow(icon: Mdi.sort_calendar_descending, label: "Year Desc")
+                          ),
+                        ];
+                      },
+                      icon: Iconify(MaterialSymbols.sort_rounded, color: Colors.white),
+                      position: PopupMenuPosition.under, color: Colors.grey.shade900,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                    ),
+                  ],
+                ),
+              ),
+              Gap(10)
+            ],
+          ),
+        )
+      ),
+    );
+  }
+
+  SliverPadding _playlistSongs(Playlist playlist) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+      sliver: SliverList.builder(
+        itemCount: playlist.songs!.length,
+        itemBuilder: (context, index) {
+          Song song = playlist.songs![index];
+          return SongsRow(song: song);
+        },
+      ),
+    );
+  }
+
+  ExpandableFab _floatingActionButton() {
+    return ExpandableFab(
+      duration: Duration(milliseconds: 250),
+      distance: 100.0,
+      type: ExpandableFabType.fan,
+      pos: ExpandableFabPos.right,
+      childrenOffset: Offset(0,0),
+      fanAngle: 60,
+      openButtonBuilder: RotateFloatingActionButtonBuilder(
+        child: Iconify(IconParkTwotone.more_four, color: accentColor),
+        fabSize: ExpandableFabSize.regular,
+        foregroundColor: accentColor,
+        backgroundColor: accentColorDark,
+        shape: CircleBorder(),
+        angle: 3.14 * 2,
+      ),
+      closeButtonBuilder: FloatingActionButtonBuilder(
+        size: 56,
+        builder: (BuildContext context, void Function()? onPressed, Animation<double> progress) {
+          return IconButton(
+            onPressed: onPressed,
+            icon: Iconify(AntDesign.close_circle_twotone, size: 40, color: accentColor),
+          );
+        },
+      ),
+      overlayStyle: ExpandableFabOverlayStyle(blur: 5),
+      children: [
+        FloatingActionButton(
+          onPressed: () {
+            Get.to(() => ToDoView(text: "Add this playlist to starred"));
+          },
+          tooltip: "Add to Playlist",
+          backgroundColor: accentColorDark, shape: CircleBorder(),
+          child: Iconify(MaterialSymbols.star_outline_rounded, color: accentColor, size: 40)
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            Get.to(() => ToDoView(text: "Add this playlist to library"));
+          },
+          tooltip: "Add to Queue",
+          backgroundColor: accentColorDark, shape: CircleBorder(),
+          child: Iconify(MaterialSymbols.add, color: accentColor, size: 40)
+        ),
+      ],
     );
   }
 }
